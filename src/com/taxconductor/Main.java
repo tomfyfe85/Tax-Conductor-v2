e48d3a369207713ba6f8b86999c52bea07b97053
tomfyfe <tomfyfe85@gmail.com>
2026-09-01 23:17:26 +0100

    add first basic tax function

--- src/com/taxconductor/Main.java
+++ src/com/taxconductor/Main.java
@@ -1,7 +1,18 @@
 package com.taxconductor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main() {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter gross earnings: ");
+
+        final float BASIC = .2F;
+
+        float gross = scanner.nextFloat();
+        float tax = gross * BASIC;
+        float net = gross - tax;
+
+        System.out.printf("Tax: %f,\nNet profit: %f\n", tax, net);
     }
 }
