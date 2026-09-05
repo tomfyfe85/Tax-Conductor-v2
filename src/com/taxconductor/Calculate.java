f5bafddcc760db9a3f88950bfa571c00dc16e06f
tomfyfe <tomfyfe85@gmail.com>
2026-09-05 09:21:10 +0100

    add calculate takes userBracket as args for emun

--- src/com/taxconductor/Calculate.java
+++ src/com/taxconductor/Calculate.java
@@ -1,13 +1,15 @@
 package com.taxconductor;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Locale;
 
 
 public class Calculate {
+    private final String user_bracket;
     double gross;
 
-    public Calculate(double gross) {
-        this.gross = gross;
+    public Calculate(double gross, String user_bracket) {
+        this.gross = gross; this.user_bracket = user_bracket;
     }
 
     private BigDecimal grossToBigDecimal() {
@@ -16,9 +18,10 @@ public class Calculate {
     }
 
     public BigDecimal tax() {
-        final BigDecimal BASIC = new BigDecimal("0.20");
+        TaxBracket currentTaxBracket = TaxBracket.valueOf(this.user_bracket.toUpperCase());
 
-        BigDecimal tax = grossToBigDecimal().multiply(BASIC);
+
+        BigDecimal tax = grossToBigDecimal().multiply(currentTaxBracket.getBracket());
         return tax.setScale(2, RoundingMode.DOWN);
     }
 
--- src/com/taxconductor/Main.java
+++ src/com/taxconductor/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter gross earnings: ");
+        double gross = scanner.nextDouble();
+        System.out.print("Enter tax bracket, IE BASIC (20%), HIGHER_RATE (40%), or ADDITIONAL(45%): ");
+        String user_bracket = scanner.next();
 
-        Calculate calculate = new Calculate(scanner.nextFloat());
+        Calculate calculate = new Calculate(gross, user_bracket);
         String tax = calculate.tax().toPlainString();
         String net = calculate.net().toPlainString();
 
